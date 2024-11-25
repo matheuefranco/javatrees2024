c2126bb7c94504d3118baaa8995fb891bec67872
matheuefranco <matheusefranco@gmail.com>
2024-11-25 09:53:29 -0300

    binary_tree

---
+++ src/javatree2024/JavaTree2024.java
@@ -0,0 +1,12 @@
+
+package javatree2024;
+
+
+public class JavaTree2024 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/javatree2024/Node.java
@@ -0,0 +1,15 @@
+
+package javatree2024;
+
+public class Node<T extends Comparable<T>> {
+    T dado;
+    Node<T> direita;
+    Node<T> esquerda;
+    
+    public Node(T novoDado){
+        this.dado = novoDado;
+        this.esquerda = null;
+        this.direita = null;
+    }
+    
+}
